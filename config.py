d841168067830bdb05d0eb83a18e7da02a7a35dc
Zepten <39930495+Zepten@users.noreply.github.com>
2021-03-01 10:14:54 +0300

    omg token -_-

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-#TOKEN = os.environ.get('BOT_TOKEN') # Токен
-TOKEN = 'ODE1MzgwMDg1OTI2MDAyNzA5.YDrj4Q.PnUsppmdrntKEceC3mBIDQjh5Oo'
+TOKEN = os.environ.get('BOT_TOKEN') # Токен
 PREFIX = "-"                        # Префикс
