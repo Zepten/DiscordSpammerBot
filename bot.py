8b77270906cfca2ba09d16826e82627415110b3f
Zepten <39930495+Zepten@users.noreply.github.com>
2021-03-01 10:13:50 +0300

    added help

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+from discord.flags import Intents
 import config
 import ipcalc
+import hello
 
 import typing
 from discord import message
@@ -11,34 +13,50 @@ from asyncio import sleep
 import random
 
 # Права для бота
-intents = discord.Intents(config.INTENTS)
-bot = commands.Bot(command_prefix = config.PREFIX, intents=intents)
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
+bot.remove_command('help')
+
+# Готовность бота к работе
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is ready!')
 
 # Help
-@bot.command()
+@bot.group(invoke_without_command=True)
 async def help(ctx):
-    text =  '**Вот что я умею:**\n'
-    text += ':wave: `-hi` - Поздороваться\n'
-    text += ':detective: `-anon` - Сказать что-то от моего имени *(твое сообщение удалится)*\n'
-    text += ':computer: `-ip` - IP-калькулятор (хз, зачем Паша его сюда вставил, мб больше нечего xd)\n'
-    text += ':yum: `-fanfic` - Рассказать мой знаменитый фанфик :heart: :blue_heart:'
-    await ctx.send(text)
+    emb = discord.Embed(title=':face_with_monocle: Для любопытных',
+        description='Напиши `-help <имя команды>` для более подробной информации')
+    emb.add_field(name='hi :wave:', value='Поздороваться')
+    emb.add_field(name='anon :detective:', value='Сказать что-то от моего имени')
+    emb.add_field(name='ip :computer:', value='IP-калькулятор')
+    await ctx.send(embed=emb)
 
 # Поздороваться
 @bot.command()
 async def hi(ctx):
     author = ctx.message.author
-    hello_word = [
-        'Привет', 'Здорова', 'Здравствуй',
-        'Моё почтение', 'Hello', 'Приветствую'
-    ]
-    await ctx.send(f'{random.choice(hello_word)}, {author.mention}')
+    await ctx.send(f'{random.choice(hello.hello_word)}, {author.mention}')
+
+@help.command()
+async def hi(ctx):
+    emb = discord.Embed(title='Hi :wave:', 
+        description='Поздороваюсь с тобой на всех языках мира по твоему желанию.\nЯ вообще все сделаю по твоему желанию :flushed:')
+    emb.add_field(name='Синтаксис', value='-hi')
+    await ctx.send(embed=emb)
 
 # Сказать от имени бота
 @bot.command()
-async def say(ctx, *, arg):
+async def anon(ctx, *, arg):
     await ctx.message.delete()
     await ctx.send(arg)
+
+@help.command()
+async def anon(ctx):
+    emb = discord.Embed(title='Anon :detective:', 
+        description='Ты вводишь сообщение, а я делаю так, чтобы никто не узнал о том, что это написал именно ты\n*P.S.: Я ничего не гарантирую :grin:*')
+    emb.add_field(name='Синтаксис', value='-anon <сообщение>')
+    await ctx.send(embed=emb)
 
 # IP-калькулятор
 @bot.command()
@@ -51,6 +69,13 @@ async def ip(ctx, *arg):
         await ctx.send('Гайд как ввести команду **ip**: ```-ip <ip-адрес> <номер маски>```')
     elif len(arg) > 2:
         await ctx.send('В глаза долбишься? Надо только 2 аргумента.')
+
+@help.command()
+async def ip(ctx):
+    emb = discord.Embed(title='IP :computer:', 
+        description='Хз, зачем Паша его сюда вставил, мб больше нечего :cry:')
+    emb.add_field(name='Синтаксис', value='-ip <ip-адрес> <номер маски>')
+    await ctx.send(embed=emb)
 
 # Запуск бота
 bot.run(config.TOKEN)
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.environ.get('BOT_TOKEN') # Токен
+#TOKEN = os.environ.get('BOT_TOKEN') # Токен
+TOKEN = 'ODE1MzgwMDg1OTI2MDAyNzA5.YDrj4Q.PnUsppmdrntKEceC3mBIDQjh5Oo'
 PREFIX = "-"                        # Префикс
-INTENTS = 4294966992                # Права
