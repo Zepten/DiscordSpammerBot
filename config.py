bfa69f6ad52e8644956e879795bb3a1a9e7c1808
Zepten <39930495+Zepten@users.noreply.github.com>
2021-03-01 08:54:16 +0300

    2-nd

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-TOKEN = os.environ.get('BOT_TOKEN')
-PREFIX = "+"
+TOKEN = os.environ.get('BOT_TOKEN') # Токен
+PREFIX = "-"                        # Префикс
+INTENTS = 4294966992                # Права
